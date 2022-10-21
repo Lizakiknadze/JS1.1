c7af04c8cb16b08260d3a1462bc7101e680ffbf4
Lizakiknadze <112972911+Lizakiknadze@users.noreply.github.com>
2022-10-21 16:30:06 +0400

    s

---
+++ script.js
@@ -0,0 +1,14 @@
+var income = 1000;
+if (income > 1500){
+    var message = "kargi xelpasi"
+    alert (message)
+}
+else{
+    alert ("norm xelpasi")
+}
+
+var arr = [1,4,18,24,15,2,3,19,55]
+for (var i=0; 1 < Array.length; i++){
+    if (i % 2 = 0)
+        console.log(arr.push(arr[i]))
+    }
