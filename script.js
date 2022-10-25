465cc2d26cf9b5c9df25a039aaf9d1fee3c54e5e
Lizakiknadze <112972911+Lizakiknadze@users.noreply.github.com>
2022-10-25 17:56:34 +0400

    ll;lll

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-var income = 1000;
-if (income > 1500){
-    var message = "kargi xelpasi"
-    alert (message)
-}
-else{
-    alert ("norm xelpasi")
-}
+// var number = ["erti", "ori", "sami", "otxi"]
+// for(var i = 0; i < number.length; i++){
+//     document.write(number[i] + "</br>")
+// }
 
-var arr = [1,4,18,24,15,2,3,19,55]
-for (var i=0; 1 < Array.length; i++){
-    if (i % 2 = 0)
-        console.log(arr.push(arr[i]))
+// var arr = [ 15,53,22,198,10,28,16,70,33,951 ];
+// for (var i = 0; i < arr.length; i++)
+// if (arr[i] % 2 == 0){
+//     console.log(arr[i] + "" + "luwi")
+// }
+
+// var arr = [ 15,53,22,198,10,28,16,70,33,951 ];
+// for (var i = 0; i < arr.length; i++)
+// if (arr[i] % 2 == 1){
+//     console.log(arr[i] + "" + "kenti")
+// }
+
+//ra xdebaaa
+function xelpasi() {
+    if (xelpasi <= 100){
+        document.write("dabali")
     }
+    else if (xelpasi > 100 && xelpasi <= 200){
+        document.write("norm")
+    }
+    else{
+        document.write("magali")
+    }
+}
+xelpasi(180)
